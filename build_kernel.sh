a18f6f59b6a000bea243b0dfcbeced1e12309306
physwizz <pollockg86@gmail.com>
2023-02-13 19:56:44 +1100

    update RREADME.md

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+make clean && make mrproper
 export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin:$PATH
 export PATH=$(pwd)/toolchain/build/build-tools/path/linux-x86:$(pwd)/toolchain/prebuilts/gas/linux-x86:$PATH
 
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- s5e8825-a33xxx_defconfig
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j32
+make PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- physwizz_defconfig
+make PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j32
